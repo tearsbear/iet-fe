64eed6c9dbbdf1956460355d836858edd500acdb
elvandiano <>
2024-03-07 15:15:47 +0700

    responsif menu project

--- config/components/desktop/project/tab-esdm.php
+++ config/components/desktop/project/tab-esdm.php
@@ -1,32 +1,32 @@
-<div class="tab-content">
+<div class="content-data" data-aos="fade-up" data-aos-duration="1300">
+    <div class="row">
+        <div class="col-lg-6 col">
+            <h3 class="color1 font-inter-semi">ESDM Project List</h3>
+        </div>
+        <div class="col-lg-6 col">
+            <div class="dropdown drop-filter">
+                <a class="btn btn-sm btn-outline-primary btn-filter" href="#" data-toggle="dropdown" aria-expanded="false">
+                    <img src="assets/icons/filter.svg" alt=""> Province
+                </a>
 
-    <h3 class="color1 font-inter-semi">ESDM Project List</h3>
-    <div class="dropdown drop-filter">
-        <a class="btn btn-outline-primary btn-filter" href="#" data-toggle="dropdown" aria-expanded="false">
-            <img src="assets/icons/filter.svg" alt=""> Province
-        </a>
-
-        <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Aceh</a>
-            <a class="dropdown-item" href="#">Jakarta</a>
-            <a class="dropdown-item" href="#">Bali</a>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#">Aceh</a>
+                    <a class="dropdown-item" href="#">Jakarta</a>
+                    <a class="dropdown-item" href="#">Bali</a>
+                </div>
+            </div>
         </div>
     </div>
-    <!-- <a href="#" class="btn btn-outline-primary"><img src="assets/icons/filter.svg" alt=""> Province</a> -->
     <div style="clear: both;"></div>
     <p class="font-inter-light text-muted mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod
-        tempor
-        incididunt ut
-        labore et dolore magna aliqua. labore et doloreasd Lorem ipsum dolor sit amet, consectetur adipiscing
-        elit, sed
-        do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. labore et doloredd</p>
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. labore et doloreasd Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. labore et doloredd</p>
     <a href="#" class="color1 font-inter-semi">Log in for more info</a>
-
-
-    <table class="table mt-4">
-        <thead>
+</div>
+<div class="table-data container testimonial-group" data-aos="fade-up" data-aos-duration="1300">
+    <div class="flex-nowrap row">
+        <table class="table mt-4">
+            <thead>
             <tr class="color1 font-inter-regular">
                 <th scope="col">Project Name</th>
                 <th scope="col">Generator Type</th>
@@ -34,18 +34,44 @@
                 <th scope="col">COD target</th>
                 <th scope="col">Developer</th>
             </tr>
-        </thead>
-        <tbody>
+            </thead>
+            <tbody>
             <tr class="text-muted font-inter-light">
                 <td>Lorem ipsum dolor sit amet</td>
-                <td style="max-width: 250px">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor</td>
+                <td style="max-width: 250px">Lorem ipsum dolor sit amet, consectetur </td>
                 <td>Lorem ipsum</td>
                 <td>Lorem ipsum</td>
                 <td>Lorem ipsum</td>
             </tr>
-
-        </tbody>
-    </table>
-
+            <tr class="text-muted font-inter-light">
+                <td>Lorem ipsum dolor sit amet</td>
+                <td style="max-width: 250px">Lorem ipsum dolor sit amet, consectetur </td>
+                <td>Lorem ipsum</td>
+                <td>Lorem ipsum</td>
+                <td>Lorem ipsum</td>
+            </tr>
+            <tr class="text-muted font-inter-light">
+                <td>Lorem ipsum dolor sit amet</td>
+                <td style="max-width: 250px">Lorem ipsum dolor sit amet, consectetur </td>
+                <td>Lorem ipsum</td>
+                <td>Lorem ipsum</td>
+                <td>Lorem ipsum</td>
+            </tr>
+            <tr class="text-muted font-inter-light">
+                <td>Lorem ipsum dolor sit amet</td>
+                <td style="max-width: 250px">Lorem ipsum dolor sit amet, consectetur </td>
+                <td>Lorem ipsum</td>
+                <td>Lorem ipsum</td>
+                <td>Lorem ipsum</td>
+            </tr>
+            <tr class="text-muted font-inter-light">
+                <td>Lorem ipsum dolor sit amet</td>
+                <td style="max-width: 250px">Lorem ipsum dolor sit amet, consectetur </td>
+                <td>Lorem ipsum</td>
+                <td>Lorem ipsum</td>
+                <td>Lorem ipsum</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
