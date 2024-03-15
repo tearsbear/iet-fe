ea116537413e1271a2b0984f0d526e6a40211830
elvandiano <>
2024-03-15 14:38:51 +0700

    fixing home

--- config/components/desktop/home/biograph.php
+++ config/components/desktop/home/biograph.php
@@ -1,32 +1,136 @@
 <div class="bg-biograph">
     <br>
-    <div class="row">
-        <div class="col-md-4 content">
-            <h1 class="text-light font-inter-bold">Luhut Binsar Pandjaitan</h1>
-            <p class="font-inter-regular role">Indonesia's Coordinating Minister for <br> Maritime Affairs and
-                Investment</p>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="row">
+                    <div class="col-md-4 content">
+                        <h1 class="text-light font-inter-bold">Luhut Binsar Pandjaitan</h1>
+                        <p class="font-inter-regular role">Indonesia's Coordinating Minister for <br> Maritime Affairs and
+                            Investment</p>
 
-            <div class="quote">
-                <img src="assets/icons/quote.svg" alt="">
-                <div>
-                    <p class="font-poppins-regular">A paradigm shift will inevitably have an impact <br> on changes in
-                        work,
-                        development scenarios, <br />
-                        business orientation and so on.</p>
+                        <div class="quote">
+                            <img src="assets/icons/quote.svg" alt="">
+                            <div>
+                                <p class="font-poppins-regular">A paradigm shift will inevitably have an impact <br> on changes in
+                                    work,
+                                    development scenarios, <br />
+                                    business orientation and so on.</p>
 
-                    <p class="font-poppins-regular">
-                        So we want justice. Those with heavy burdens <br /> must be assisted, (countries) that are ready
-                        to <br /> go their own way, in addition to helping those <br /> who are not able to.
-                    </p>
-                    <p class="font-poppins-regular">
-                        We must be fully supported by strong global <br /> cooperation, which we will build at G20
-                        Indonesia. <br /> This is what we mean by a global deal.
-                    </p>
+                                <p class="font-poppins-regular">
+                                    So we want justice. Those with heavy burdens <br /> must be assisted, (countries) that are ready
+                                    to <br /> go their own way, in addition to helping those <br /> who are not able to.
+                                </p>
+                                <p class="font-poppins-regular">
+                                    We must be fully supported by strong global <br /> cooperation, which we will build at G20
+                                    Indonesia. <br /> This is what we mean by a global deal.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-5">
+                        <img class="img-profile" src="assets/image/luhut.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="row">
+                    <div class="col-md-4 content">
+                        <h1 class="text-light font-inter-bold">Erick Thohir</h1>
+                        <p class="font-inter-regular role">Minister of State-owned <br> enterprises of Indonesia</p>
+
+                        <div class="quote">
+                            <img src="assets/icons/quote.svg" alt="">
+                            <div>
+                                <p class="font-poppins-regular">A paradigm shift will inevitably have an impact <br> on changes in
+                                    work,
+                                    development scenarios, <br />
+                                    business orientation and so on.</p>
+
+                                <p class="font-poppins-regular">
+                                    So we want justice. Those with heavy burdens <br /> must be assisted, (countries) that are ready
+                                    to <br /> go their own way, in addition to helping those <br /> who are not able to.
+                                </p>
+                                <p class="font-poppins-regular">
+                                    We must be fully supported by strong global <br /> cooperation, which we will build at G20
+                                    Indonesia. <br /> This is what we mean by a global deal.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-5">
+                        <img class="img-profile" src="assets/image/erick.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="row">
+                    <div class="col-md-4 content">
+                        <h1 class="text-light font-inter-bold">Sri Mulyani</h1>
+                        <p class="font-inter-regular role">Indonesia Minister Of Finance</p>
+
+                        <div class="quote">
+                            <img src="assets/icons/quote.svg" alt="">
+                            <div>
+                                <p class="font-poppins-regular">A paradigm shift will inevitably have an impact <br> on changes in
+                                    work,
+                                    development scenarios, <br />
+                                    business orientation and so on.</p>
+
+                                <p class="font-poppins-regular">
+                                    So we want justice. Those with heavy burdens <br /> must be assisted, (countries) that are ready
+                                    to <br /> go their own way, in addition to helping those <br /> who are not able to.
+                                </p>
+                                <p class="font-poppins-regular">
+                                    We must be fully supported by strong global <br /> cooperation, which we will build at G20
+                                    Indonesia. <br /> This is what we mean by a global deal.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-5">
+                        <img class="img-profile" src="assets/image/sri.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="row">
+                    <div class="col-md-4 content">
+                        <h1 class="text-light font-inter-bold">Arifin Tasrif</h1>
+                        <p class="font-inter-regular role">Indonesia Minister of Energy and <br>Mineral Resources</p>
+
+                        <div class="quote">
+                            <img src="assets/icons/quote.svg" alt="">
+                            <div>
+                                <p class="font-poppins-regular">A paradigm shift will inevitably have an impact <br> on changes in
+                                    work,
+                                    development scenarios, <br />
+                                    business orientation and so on.</p>
+
+                                <p class="font-poppins-regular">
+                                    So we want justice. Those with heavy burdens <br /> must be assisted, (countries) that are ready
+                                    to <br /> go their own way, in addition to helping those <br /> who are not able to.
+                                </p>
+                                <p class="font-poppins-regular">
+                                    We must be fully supported by strong global <br /> cooperation, which we will build at G20
+                                    Indonesia. <br /> This is what we mean by a global deal.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-5">
+                        <img class="img-profile" src="assets/image/arifin.png" alt="">
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-5">
-            <img class="img-profile" src="assets/image/luhut.png" alt="">
-        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
 </div>
