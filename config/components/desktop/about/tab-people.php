c2e51a4ad177efd17870066e33732e5c0f1e547c
elvandiano <>
2024-03-18 15:00:21 +0700

    about page

--- config/components/desktop/about/tab-people.php
+++ config/components/desktop/about/tab-people.php
@@ -1,19 +1,141 @@
 <div class="text-center">
     <h3 class="font-inter-bold color1" style="margin-top: 80px">People</h3>
 </div>
-<div class="row row-people text-center">
-    <div class="col-lg-3" data-aos="fade-up" data-aos-duration="1300">
-        <img src="assets/people/yusuf.png" alt="">
-        <h5 class="font-inter-medium text-primary">
-            Yusuf Didi Setiarto
-        </h5>
-        <p class="text-muted font-inter-light">Deputy of IET</p>
-    </div>
-    <div class="col-lg-3" data-aos="fade-up" data-aos-duration="1300">
-        <img src="assets/people/edo.png" alt="">
+<div class="row row-people justify-content-center">
+    <div class="col-lg-3" align="center" data-aos="fade-up" data-aos-duration="1300">
+        <img src="assets/people/edo.png" style="max-height:280px" alt="">
         <h5 class="font-inter-medium text-primary">
             Edo Mahendra
         </h5>
-        <p class="text-muted font-inter-light">Head of IET</p>
+        <p class="text-muted font-inter-light">Chairperson</p>
+    </div>
+    <div class="col-lg-3" align="center" data-aos="fade-up" data-aos-duration="1300">
+        <img src="assets/people/yusuf.png" style="max-height:280px" alt="">
+        <h5 class="font-inter-medium text-primary">
+            Yusuf Didi Setiarto
+        </h5>
+        <p class="text-muted font-inter-light">Vice Chairperson</p>
+    </div>
+</div>
+<div class="row row-people justify-content-center">
+    <div class="col-lg-3" align="center" data-aos="fade-up" data-aos-duration="1300">
+        <img src="assets/people/404foto.png" width="250px" style="border-radius: 10px" alt="">
+        <h5 class="font-inter-medium text-primary">
+            Yudo Dwinanda Priaadi
+        </h5>
+        <p class="text-muted font-inter-light">Advisor</p>
+    </div>
+</div>
+<div class="row row-people justify-content-center">
+    <div class="col-lg-3" align="center" data-aos="fade-up" data-aos-duration="1300">
+        <img src="assets/people/404foto.png" width="250px" style="border-radius: 10px" alt="">
+        <h5 class="font-inter-medium text-primary">
+            Achmad Firdaus
+        </h5>
+        <p class="text-muted font-inter-light">Energy Transition & Sustainable Projects Specialist</p>
+    </div>
+    <div class="col-lg-3" align="center" data-aos="fade-up" data-aos-duration="1300">
+        <img src="assets/people/404foto.png" width="250px" style="border-radius: 10px" alt="">
+        <h5 class="font-inter-medium text-primary">
+            Ady Setya Nugroho
+        </h5>
+        <p class="text-muted font-inter-light">System Planning Specialist</p>
+    </div>
+    <div class="col-lg-3" align="center" data-aos="fade-up" data-aos-duration="1300">
+        <img src="assets/people/alfredd.png" style="max-height:280px" alt="">
+        <h5 class="font-inter-medium text-primary">
+            Alfred Sirait
+        </h5>
+        <p class="text-muted font-inter-light">Energy Transition Policy & Legal Specialist</p>
+    </div>
+    <div class="col-lg-3" align="center" data-aos="fade-up" data-aos-duration="1300">
+        <img src="assets/people/404foto.png" width="250px" style="border-radius: 10px" alt="">
+        <h5 class="font-inter-medium text-primary">
+            Allan M. Ardhian
+        </h5>
+        <p class="text-muted font-inter-light">System Planning Specialist</p>
+    </div>
+</div>
+<div class="row row-people justify-content-center">
+    <div class="col-lg-3" align="center" data-aos="fade-up" data-aos-duration="1300">
+        <img src="assets/people/404foto.png" width="250px" style="border-radius: 10px" alt="">
+        <h5 class="font-inter-medium text-primary">
+            Angela P. Mangeka
+        </h5>
+        <p class="text-muted font-inter-light">Energy & Sustainable Finance Specialist</p>
+    </div>
+    <div class="col-lg-3" align="center" data-aos="fade-up" data-aos-duration="1300">
+        <img src="assets/people/404foto.png" width="250px" style="border-radius: 10px" alt="">
+        <h5 class="font-inter-medium text-primary">
+            Antonio F. Maha
+        </h5>
+        <p class="text-muted font-inter-light">ESG & Energy Transition Specialist</p>
+    </div>
+    <div class="col-lg-3" align="center" data-aos="fade-up" data-aos-duration="1300">
+        <img src="assets/people/404foto.png" width="250px" style="border-radius: 10px" alt="">
+        <h5 class="font-inter-medium text-primary">
+            Debby Panjaitan
+        </h5>
+        <p class="text-muted font-inter-light">Energy Transition Policy and Legal SpecialistT</p>
+    </div>
+    <div class="col-lg-3" align="center" data-aos="fade-up" data-aos-duration="1300">
+        <img src="assets/people/404foto.png" width="250px" style="border-radius: 10px" alt="">
+        <h5 class="font-inter-medium text-primary">
+            Fajar B. Suryawan
+        </h5>
+        <p class="text-muted font-inter-light">Energy Transition & Sustainable Projects Specialist</p>
+    </div>
+</div>
+<div class="row row-people justify-content-center">
+    <div class="col-lg-3" align="center" data-aos="fade-up" data-aos-duration="1300">
+        <img src="assets/people/gafar.png" style="max-height:280px" alt="">
+        <h5 class="font-inter-medium text-primary">
+            Gafar Ali
+        </h5>
+        <p class="text-muted font-inter-light">Corporate Planning Specialist</p>
+    </div>
+    <div class="col-lg-3" align="center" data-aos="fade-up" data-aos-duration="1300">
+        <img src="assets/people/gita.png" style="max-height:280px" alt="">
+        <h5 class="font-inter-medium text-primary">
+            Gitta A. Septiani
+        </h5>
+        <p class="text-muted font-inter-light">Energy & Sustainable Finance Specialist</p>
+    </div>
+    <div class="col-lg-3" align="center" data-aos="fade-up" data-aos-duration="1300">
+        <img src="assets/people/marcel.png" style="max-height:280px" alt="">
+        <h5 class="font-inter-medium text-primary">
+            Marcellina Laurensia
+        </h5>
+        <p class="text-muted font-inter-light">Executive Assistant</p>
+    </div>
+    <div class="col-lg-3" align="center" data-aos="fade-up" data-aos-duration="1300">
+        <img src="assets/people/dani.jpg" style="max-height:280px" alt="">
+        <h5 class="font-inter-medium text-primary">
+            M. Dani Gitatisna
+        </h5>
+        <p class="text-muted font-inter-light">Corporate Planning Specialist</p>
+    </div>
+</div>
+<div class="row row-people justify-content-center">
+    <div class="col-lg-3" align="center" data-aos="fade-up" data-aos-duration="1300">
+        <img src="assets/people/404foto.png" width="250px" style="border-radius: 10px" alt="">
+        <h5 class="font-inter-medium text-primary">
+            Nisa Hadid
+        </h5>
+        <p class="text-muted font-inter-light">ESG & Energy Transition Specialist</p>
+    </div>
+    <div class="col-lg-3" align="center" data-aos="fade-up" data-aos-duration="1300">
+        <img src="assets/people/Putu.jpeg" style="max-height:280px" alt="">
+        <h5 class="font-inter-medium text-primary">
+            Putu Purwaningsih
+        </h5>
+        <p class="text-muted font-inter-light">Energy Transition Policy & Legal Specialist</p>
+    </div>
+    <div class="col-lg-3" align="center" data-aos="fade-up" data-aos-duration="1300">
+        <img src="assets/people/ronald.png" style="max-height:280px" alt="">
+        <h5 class="font-inter-medium text-primary">
+            Ronald Ambatoho
+        </h5>
+        <p class="text-muted font-inter-light">Energy Transition & Sustainable Projects Specialist</p>
     </div>
 </div>
--- config/components/desktop/navbar/navbar.php
+++ config/components/desktop/navbar/navbar.php
@@ -4,31 +4,31 @@
             <img src="assets/logo-alt.svg" alt="">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item nav-space">
-                    <a class="nav-link link-item <?php 
-    echo ((!isset($_GET['page']) || $_GET['page'] == 'home') ? 'nav-active' : 'nav-unactive');
-?>" href="index.php?page=home" style="margin-top: -1px">Home</a>
+                    <a class="nav-link link-item <?php
+                    echo ((!isset($_GET['page']) || $_GET['page'] == 'home') ? 'nav-active' : 'nav-unactive');
+                    ?>" href="index.php?page=home" style="margin-top: -1px">Home</a>
                 </li>
                 <li class="nav-item nav-space">
                     <a class="nav-link link-item <?php echo (isset($_GET['page']) && $_GET['page'] == 'about') ? 'nav-active' : 'nav-unactive'; ?>"
-                        href="index.php?page=about">About IET</a>
+                       href="index.php?page=about">About IET</a>
                 </li>
                 <li class="nav-item nav-space">
                     <a class="nav-link link-item <?php echo (isset($_GET['page']) && $_GET['page'] == 'news') ? 'nav-active' : 'nav-unactive'; ?>"
-                        href="index.php?page=news">News & Events</a>
+                       href="index.php?page=news">News & Events</a>
                 </li>
                 <li class="nav-item nav-space">
                     <a class="nav-link link-item <?php echo (isset($_GET['page']) && $_GET['page'] == 'project') ? 'nav-active' : 'nav-unactive'; ?>"
-                        href="index.php?page=project">Projects</a>
+                       href="index.php?page=project">Projects</a>
                 </li>
-                <li class="nav-item nav-space dropdown">
+                <li class="nav-item nav-space dropdown opt-lang">
                     <a class="nav-link font-inter-regular nav-drop" style="color: #fff;" href="#" role="button"
-                        data-toggle="dropdown" aria-expanded="false">
+                       data-toggle="dropdown" aria-expanded="false">
                         <img src="assets/icons/globe-white.svg" style="vertical-align: sub;" alt=""> EN
                     </a>
                     <div class="dropdown-menu">
@@ -38,13 +38,35 @@
                 </li>
                 <li class="nav-item">
                     <form class="form-inline">
-                        <div class="form-control input-search mr-sm-2">
+                        <div class="form-control input-search">
                             <input type="text" class="font-poppins-regular" placeholder="Search" />
+                            <img class="icon-search" src="assets/icons/search.svg" aria-hidden="true" alt="">
                         </div>
-                        <img class="icon-search" src="assets/icons/search.svg" aria-hidden="true" alt="">
-                        <a href="index.php?page=login" class="btn btn-login">Login</a>
+                        <!-- Sebelum Login -->
+
+                        <!-- <a href="index.php?page=login" class="btn btn-login">Login</a> -->
+
+                        <!-- Sebelum Login -->
                     </form>
                 </li>
+                <!-- Setelah Login -->
+
+                <li class="nav-item nav-space dropdown">
+                    <a class="font-inter-regular" style="color: #fff;" href="#" role="button"
+                       data-toggle="dropdown" >
+                        <img src="assets/icons/profile.svg" style="padding: 8px" alt="Profile" class="profile">
+                    </a>
+                    <div class="dropdown-menu drop-prof">
+                        <a class="dropdown-item drop-sel" style="font-size: 13px" href="#">
+                            <img src="assets/icons/Dashboard.svg" alt="dash-icon">Dashboard Project Owner</a>
+                        <a class="dropdown-item drop-sel" href="#" style="font-size: 13px">
+                            <img src="assets/icons/pencil-alt.svg" alt="dash-icon">Edit Profile</a>
+                        <a class="dropdown-item drop-sel" href="#" style="font-size: 13px">
+                            <img src="assets/icons/logout.svg" alt="dash-icon">Logout</a>
+                    </div>
+                </li>
+
+                <!-- Setelah Login -->
             </ul>
         </div>
     </div>
