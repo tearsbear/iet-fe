2add800f27159ff9e650da7861d7d66c7a737ec9
elvandiano <>
2024-03-20 15:26:11 +0700

    fixing carousel

--- config/components/desktop/home/biograph.php
+++ config/components/desktop/home/biograph.php
@@ -1,6 +1,12 @@
 <div class="bg-biograph">
     <br>
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleControls" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleControls" data-slide-to="1"></li>
+            <li data-target="#carouselExampleControls" data-slide-to="2"></li>
+            <li data-target="#carouselExampleControls" data-slide-to="3"></li>
+        </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <div class="row">
